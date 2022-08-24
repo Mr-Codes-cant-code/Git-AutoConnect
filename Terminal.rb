aa1c8e28f26e9cce3960e9899d0e94872dced30b
Khalid Azmatullah <khalidazy.2008@gmail.com>
2022-08-24 16:35:52 +0530

    Terminal Update

--- Terminal.rb
+++ Terminal.rb
@@ -2,19 +2,6 @@ connection =false
 puts(">>>   Auto-GitProcess   <<<")
 if connection == false
   require_relative "Library/Functions/auto-connect.rb"
-
-  #change_fold_name
-  fold_pwd = Dir.pwd
-  fold_pwd = fold_pwd.split("/")
-  curr_fold = fold_pwd[-1]
-  if curr_fold == "Auto-GitConnect"
-    require 'fileutils'
-    auto_gitprocess = Dir.pwd
-    new_file = File.read("Library/Words/repo-name.txt")
-    new_file = new_file.strip()
-    FileUtils.mv auto_gitprocess, new_file
-  end
-
   puts("Connected Succesfully!")
 end
 x = 1
